68b63955f7b2b68700f65bb0e70b8ccc732024ec
DIGYSKY <lilyan.chauveau@gmail.com>
2023-11-20 11:30:59 +0100

    add MVC

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
 <?php
 
-?>
+session_start();
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/styles.css">
-    <title>MEMENTO</title>
-</head>
-<body>
-    <header>
-        <ul>
-            <li>MEMENTO</li>
-            <li>Mon Compte</li>
-        </ul>
-    </header>
-    <main>
-        
-    </main>
-</body>
-</html>
+$aviableRouts = ['home'];
+
+$route = 'home';
+
+
+if (isset($_GET['page']) && in_array($_GET['page'], $aviableRouts)) {
+    $route = $_GET['page'];
+}
+
+require './view/layout.phtml';
