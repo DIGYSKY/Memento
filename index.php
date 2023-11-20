fc07c6a878b6aad9b87625c9012f045ae9054076
DIGYSKY <lilyan.chauveau@gmail.com>
2023-11-20 10:16:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MEMENTO</title>
+</head>
+<body>
+    <header>
+        
+    </header>
+    <main>
+        
+    </main>
+</body>
+</html>
