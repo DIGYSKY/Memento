843c6f4cb7f8e90b8f32378e7fdd35fc09786bba
DIGYSKY <lilyan.chauveau@gmail.com>
2023-11-20 12:12:29 +0100

    Add database

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 session_start();
 
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=memento;charset=utf8','root','');
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+    catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
 $aviableRouts = ['home'];
 
 $route = 'home';
