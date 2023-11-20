a6c79cd818caceabfd2519c37a9ce1ad37423127
DIGYSKY <lilyan.chauveau@gmail.com>
2023-11-20 14:22:26 +0100

    update MVC

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -1,4 +1,23 @@
 <?php
 
+$query = 'SELECT * FROM post ORDER BY modified_at ASC';
+$response = $bdd->query($query);
+$datas = $response->fetchAll();
+
+function tab_post($datas) {
+    for ($i=0; $i < count($datas); $i++) {
+        if ($datas[$i]['color'] != null) {
+            $color = $datas[$i]['color'];
+        } else {
+            $color = 'black';
+        }
+        echo '<div>';
+        echo '<p class"' . $i . '-title">' . $datas[$i]['title'] . '</p>';
+        echo '<br>';
+        echo '<p style="color: ' . $color . '">' . $datas[$i]['contents'] . '</p>';
+        echo '<br>';
+        echo '</div>';
+    }
+}
 
 require './view/home.phtml';
