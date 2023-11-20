81bf8f1f41ea3621f2d0d4db54e7e28d22348b1b
DIGYSKY <lilyan.chauveau@gmail.com>
2023-11-20 10:27:51 +0100

    ajout css

--- index.php
+++ index.php
@@ -7,11 +7,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
     <title>MEMENTO</title>
 </head>
 <body>
     <header>
-        
+        <ul>
+            <li>MEMENTO</li>
+            <li>Mon Compte</li>
+        </ul>
     </header>
     <main>
         
