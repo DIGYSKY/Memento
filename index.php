a47d6a21f9bd22aea816eeec19c85b2ec2092620
DIGYSKY <lilyan.chauveau@gmail.com>
2023-11-21 17:27:56 +0100

    Verson 1.0 final

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 
-session_start();
-
-// unset($_SESSION);
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=memento;charset=utf8','root','');
-    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-    catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
+require './controllers/connexion_controller.php';
 
 $aviableRouts = ['home', 'delete', 'account', 'newregister', 'new'];
 
